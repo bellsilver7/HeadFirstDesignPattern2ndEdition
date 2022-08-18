2fbcf317733edc007140a89206c08662152d2ea8
bellsilver7 <bellsilver7@gmail.com>
2022-08-19 06:55:54 +0900

    팩토리 패턴 / 피자 가게

---
+++ src/factory/NYCheesePizza.java
@@ -0,0 +1,11 @@
+package factory;
+
+public class NYCheesePizza extends Pizza {
+    public NYCheesePizza() {
+        name = "뉴욕 스타일 소스와 치즈 피자";
+        dough = "씬 크러스트 도우";
+        sauce = "마리나라 소스";
+
+        toppings.add("잘게 썬 레지아노 치즈");
+    }
+}
---
+++ src/factory/NYPizzaStore.java
@@ -0,0 +1,11 @@
+package factory;
+
+public class NYPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        if (type.equals("cheese")) {
+            return new NYCheesePizza();
+        }
+        return null;
+    }
+}
